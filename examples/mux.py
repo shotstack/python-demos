38139de981a3864b2bc4719c029c9c7937ff9960
Jeff Shillitto <jeff@shotstack.io>
2022-12-09 20:10:50 +1000

    Add Mux destination example

---
+++ examples/mux.py
@@ -0,0 +1,110 @@
+# Mux Demo.
+# Ensure Mux credentials are configured in the Shotstack dashboard before running this demo.
+# See: https://shotstack.io/docs/guide/serving-assets/destinations/mux
+
+import shotstack_sdk as shotstack
+import os
+
+from shotstack_sdk.model.soundtrack import Soundtrack
+from shotstack_sdk.model.image_asset import ImageAsset
+from shotstack_sdk.api import edit_api
+from shotstack_sdk.model.clip import Clip
+from shotstack_sdk.model.track import Track
+from shotstack_sdk.model.timeline import Timeline
+from shotstack_sdk.model.output import Output
+from shotstack_sdk.model.edit import Edit
+from shotstack_sdk.model.mux_destination import MuxDestination
+from shotstack_sdk.model.mux_destination_options import MuxDestinationOptions
+from shotstack_sdk.model.shotstack_destination import ShotstackDestination
+
+if __name__ == "__main__":
+    host = "https://api.shotstack.io/stage"
+
+    if os.getenv("SHOTSTACK_HOST") is not None:
+        host = os.getenv("SHOTSTACK_HOST")
+
+    configuration = shotstack.Configuration(host = host)
+
+    if os.getenv('SHOTSTACK_KEY') is None:
+        sys.exit("API Key is required. Set using: export SHOTSTACK_KEY=your_key_here")  
+
+    configuration.api_key['DeveloperKey'] = os.getenv('SHOTSTACK_KEY')
+
+    with shotstack.ApiClient(configuration) as api_client:
+        api_instance = edit_api.EditApi(api_client)
+
+        images = [
+            "https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-712850.jpeg",
+            "https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-867452.jpeg",
+            "https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/examples/images/pexels/pexels-photo-752036.jpeg",
+        ]
+
+        clips = []
+        start = 0.0
+        length = 4.0
+
+        soundtrack = Soundtrack(
+            src = "https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/music/gangsta.mp3",
+            effect = "fadeInFadeOut",
+            volume = 1.0,
+        )
+
+        for image in images:
+            imageAsset = ImageAsset(src = image)
+
+            clip = Clip(
+                asset = imageAsset,
+                start = start,
+                length = length,
+                effect = "zoomIn"
+            )
+
+            start = start + length
+            clips.append(clip)
+
+        track = Track(clips = clips)
+
+        timeline = Timeline(
+            background = "#000000",
+            soundtrack = soundtrack,
+            tracks = [track]
+        )
+
+        # Exclude from Shotstack hosting
+        shotstack_destination = ShotstackDestination(
+            exclude = True
+        )
+
+        # Send to Mux hosting
+        mux_destination_options = MuxDestinationOptions(
+            playback_policy = ["public"]
+        )
+        mux_destination = MuxDestination(
+            options = mux_destination_options
+        )
+
+        output = Output(
+            format = "mp4",
+            resolution = "sd",
+            destinations = [
+                shotstack_destination,
+                mux_destination
+            ]
+        )
+
+        edit = Edit(
+            timeline = timeline,
+            output = output
+        )
+
+        try:
+            api_response = api_instance.post_render(edit)
+
+            message = api_response['response']['message']
+            id = api_response['response']['id']
+
+            print(f"{message}\n")
+            print(">> Now check the progress of your render by running:")
+            print(f">> python examples/status.py {id}")
+        except Exception as e:
+            print(f"Unable to resolve API call: {e}")
